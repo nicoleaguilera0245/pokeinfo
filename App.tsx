bf748c7520ee6385f44a2aee693af4bce81bff1c
unknown <nicole_24736@ETEC.FURLAN>
2025-11-26 22:10:20 -0300

    Updating App.tsx + SplashScreen.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,46 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import 'react-native-gesture-handler';
+import { Ionicons } from "@expo/vector-icons";
 import SplashScreen from './src/screens/SplashScreen';
 import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function BottomTabs() {
+  return (
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      headerShown: false,
+      tabBarStyle: { backgroundColor: "#f8f8f8", height: 60 },
+      tabBarActiveTintColor: "#ffd400",
+      tabBarInactiveTintColor: "#333",
+      tabBarIcon: ({ color, size }) => {
+        let iconName: keyof typeof Ionicons.glyphMap;
+        if (route.name === "Home") {
+          iconName = "home";
+        } else {
+          iconName = "list";
+        }
+        return <Ionicons name={iconName} size={size} color={color} />
+      }
+    })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Main" component={BottomTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   )
